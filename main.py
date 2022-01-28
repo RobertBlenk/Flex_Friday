a36dba44795ca71662b287722b25f00084bb1a76
RobertBlenk <robert.blenk@baxter-academy.org>
2022-01-28 10:17:10 -0500

    setting up the window

---
+++ main.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+def main():
+
+
+
+
+
+
+    window = Tk()
+    window.title("FlashCards")
+    window.geometry("1250x405+5+5")
+    window.resizable(True, True)
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
+
