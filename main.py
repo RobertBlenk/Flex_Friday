4375ab41437cf0fe5241d36baebb8046b1f411df
RobertBlenk <robert.blenk@baxter-academy.org>
2022-03-25 18:30:00 -0400

    completed draft1, except for the actual flashcard portion. 3/25/22

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ c = conn.cursor()
 
 #  conn.commit()
 #  conn.close()
-c.execute("SELECT * FROM korean WHERE english='hello'")
-print(c.fetchone())
+#c.execute("SELECT * FROM korean WHERE english='hello'")
+#print(c.fetchone())
 
 
 def main():
@@ -29,15 +29,14 @@ def main():
 
     def entered():
         var_hangul = str(t1.get())
-        #  print(var_hangul)
         t1.delete(0, 'end')
+
         var_english = t1a.get()
-        #  print(var_english)
         t1a.delete(0, 'end')
+
         var_romaja = t1b.get()
-        #  print(var_romaja)
         t1b.delete(0, 'end')
-        #  print(var_category)
+
         Word = Input_word(var_hangul, var_romaja, var_english, var_category)
 
         c.execute("INSERT INTO korean VALUES (?, ?, ?, ?)", (Word.hangul, Word.romaja, Word.english, Word.category))
@@ -47,7 +46,6 @@ def main():
     def tb():
         global var_category
         var_category = "Basic"
-        print("pressed tb")
         t_b.config(relief=SUNKEN)
         t_n.config(relief=RAISED)
         t_v.config(relief=RAISED)
@@ -55,7 +53,6 @@ def main():
     def tn():
         global var_category
         var_category = "Noun"
-        print("pressed tn")
         t_b.config(relief=RAISED)
         t_n.config(relief=SUNKEN)
         t_v.config(relief=RAISED)
@@ -63,7 +60,6 @@ def main():
     def tv():
         global var_category
         var_category = "Verb"
-        print("pressed tv")
         t_b.config(relief=RAISED)
         t_n.config(relief=RAISED)
         t_v.config(relief=SUNKEN)
@@ -80,18 +76,18 @@ def main():
         t1a = Entry(window_inp)
         t1b = Entry(window_inp)
         t2 = Button(window_inp, text="Enter", command=entered)
-        t3 = Label(window_inp, text="Enter word in Hangul here")
-        t3a = Label(window_inp, text="Enter word in English here")
-        t3b = Label(window_inp, text="Enter the word in Romaja here")
+        t3 = Label(window_inp, text="Enter word in Hangul here:")
+        t3a = Label(window_inp, text="Enter word in English here:")
+        t3b = Label(window_inp, text="Enter the word in Romaja here:")
         t3c = Label(window_inp, text="choose a category here")
         t_b = Button(window_inp, text="Basic", command=tb)
-        t_n = Button(window_inp, text="Nouns", command=tn)
-        t_v = Button(window_inp, text="Verbs", command=tv)
+        t_n = Button(window_inp, text="Noun", command=tn)
+        t_v = Button(window_inp, text="Verb", command=tv)
         t1.place(x=20, y=22)
         t1a.place(x=20, y=72)
         t1b.place(x=20, y=112)
         t_b.place(x=20, y=162)
-        t_n.place(x=60, y=162)
+        t_n.place(x=63, y=162)
         t_v.place(x=108, y=162)
         t2.place(x=20, y=212)
         t3.place(x=20, y=0)
@@ -99,15 +95,47 @@ def main():
         t3b.place(x=20, y=90)
         t3c.place(x=20, y=140)
         window_inp.title("Input screen")
-        window_inp.geometry("850x300+5+5")
+        window_inp.geometry("450x300+5+5")
         window_inp.resizable(False, False)
         window_inp.mainloop()
 
     def change_language():
         pass
 
-    def change_difficulty():
-        pass
+    def mode_basic():
+        c.execute("SELECT * FROM korean WHERE category='Basic'")
+        xlist = list(c.fetchall())
+        conn.commit()
+        c.close()
+        return xlist
+
+    def mode_noun():
+        c.execute("SELECT * FROM korean WHERE category='Nouns'")
+        xlist = list(c.fetchall())
+        conn.commit()
+        c.close()
+        return xlist
+
+    def mode_verb():
+        c.execute("SELECT * FROM korean WHERE category='Verbs'")
+        xlist = list(c.fetchall())
+        conn.commit()
+        c.close()
+        return xlist
+
+    def change_mode():
+        window_mode = Tk()
+        c2 = Button(window_mode, text="Basic", command=mode_basic)
+        c3 = Button(window_mode, text="Noun", command=mode_noun)
+        c4 = Button(window_mode, text="Verb", command=mode_verb)
+
+        c2.place(x=0, y=0)
+        c3.place(x=100, y=0)
+        c4.place(x=200, y=0)
+        window_mode.title("Mode change")
+        window_mode.geometry("400x405+5+5")
+        window_mode.resizable(False, False)
+        window_mode.mainloop()
 
     def page_setup():
         pass
@@ -117,12 +145,12 @@ def main():
 
         a1 = Button(window_esc, text="input word", command=input_word)
         b1 = Button(window_esc, text="change language", command=change_language)
-        c1 = Button(window_esc, text="change difficulty", command=change_difficulty)
+        c1 = Button(window_esc, text="change mode", command=change_mode)
         d1 = Button(window_esc, text="page setup", command=page_setup)
 
         a1.place(x=0, y=0)
         b1.place()
-        c1.place()
+        c1.place(x=81.5, y=0)
         d1.place()
 
         window_esc.title("Escape")
@@ -131,7 +159,8 @@ def main():
         window_esc.mainloop()
 
     def flip():
-        pass
+        print("flipped")
+    # main starts
 
     e1 = Button(window_main, text="Esc", command=esc_command)
     e2 = Button(window_main, text="Flip", command=flip)
